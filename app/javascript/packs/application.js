136207fa851ae9f5a2237c949ab7595bce428402
Fernando AntÃunez <fernandojesusantunez@gmail.com>
2021-10-22 10:51:59 -0400

    Se agregaron estilos

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 import 'bootstrap';
 // import { loadDynamicBannerText } from '../components/banner';
 import AOS from 'aos';
-import { initTyped } from './plugins/init_typedjs';
+import { loadDynamicBannerText } from './plugins/init_typedjs';
 import { initSelect2 } from './plugins/init_select2';
 import { initStarRating } from './plugins/init_star_rating';
 
@@ -18,10 +18,10 @@ document.addEventListener('turbolinks:load', () => {
     // Call your JS functions here
     // [...]
     initSelect2();
-    initTyped();
+    loadDynamicBannerText();
     AOS.init();
     initStarRating();
-    // loadDynamicBannerText();
+    
   });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/packs/plugins/init_typedjs.js
+++ app/javascript/packs/plugins/init_typedjs.js
@@ -1,16 +1,13 @@
 import Typed from 'typed.js';
 
-const initTyped = () => {
-  const element = document.querySelector('.typed-js');
-  if (element) {
-    new Typed('.typed-js', {
-      strings: ["Enjoy", "Have Fun", "Shared"],
-      typeSpeed: 40,
-      loop: true,
-      loopCount: Infinity,
-      showCursor: false
-    });
-  }
-};
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["Change your life", "Learn to code"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
 
-export { initTyped };
+export { loadDynamicBannerText };
+
+// export { initTyped };
